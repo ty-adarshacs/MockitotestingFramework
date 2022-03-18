97b8eb721c6eaaa33a4b8ce918dbc6c6bf5bc29c
ty-adarshacs <82380860+ty-adarshacs@users.noreply.github.com>
2022-03-18 17:16:45 +0530

    Update SpringBootActuatorsApplication.java

--- SpringBoot_Actuators/src/main/java/com/example/demo/SpringBootActuatorsApplication.java
+++ SpringBoot_Actuators/src/main/java/com/example/demo/SpringBootActuatorsApplication.java
@@ -11,11 +11,6 @@ public class SpringBootActuatorsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActuatorsApplication.class, args);
 		System.out.println("Running Spring boot Actuators");
-<<<<<<< HEAD
-=======
-		System.out.println("Spring boot Actuator working fine ");
-		//adarsha CS
->>>>>>> parent of 4e451b4 (Merge branch 'main' of https://github.com/ty-adarshacs/MockitotestingFramework)
-		
+
 		
 }}
